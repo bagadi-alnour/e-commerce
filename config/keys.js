52b18400643e884418376aef5fa84a54912234c6
bagadi-alnour <bagadi.alnour@gmail.com>
2020-06-08 02:23:21 +0200

    add

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 // Mongodb Info
 module.exports = {
-  mongoURI: "",
-  jwt_secret: "",
+  mongoURI:
+    "mongodb+srv://bgaato:Work_2020@portfolio-ceyoc.mongodb.net/e-commerce?retryWrites=true&w=majority",
+  jwt_secret: "my_secret_token",
 };
